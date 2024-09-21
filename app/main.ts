c69328e194dc8cf12c6df102dc3dab93e6becd89
RitulBhatnagar <ritulbhatnagar375@gmail.com>
2024-09-21 12:43:23 +0530

    added the digit match

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+export {}; // This makes the file a module
+// import {Bun} from "bun";
 const args = process.argv;
 const pattern = args[3];
 
@@ -6,8 +8,8 @@ const inputLine: string = await Bun.stdin.text();
 function matchPattern(inputLine: string, pattern: string): boolean {
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
-  } else {
-    throw new Error(`Unhandled pattern: ${pattern}`);
+  } else if (pattern === "\\d") {
+    return inputLine.match(/\d/) !== null;
   }
 }
 
@@ -16,12 +18,13 @@ if (args[2] !== "-E") {
   process.exit(1);
 }
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 if (matchPattern(inputLine, pattern)) {
+  // console.log("pattern matched");
   process.exit(0);
 } else {
+  // console.log("pattern did not match");
   process.exit(1);
 }
